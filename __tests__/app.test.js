864f42cdee7d8a6d81f9a3c7ddb80a6b54726f82
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-03-15 15:18:59 -0700

    added app route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Book = require('../lib/models/Book');
 
 describe('any-old-api routes', () => {
   beforeEach(() => {
@@ -11,4 +12,6 @@ describe('any-old-api routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it
 });
