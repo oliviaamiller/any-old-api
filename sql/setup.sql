5e7898c63ec96a7ee69881389ed672e7d0247300
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-03-15 15:10:25 -0700

    books table in setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,9 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+CREATE TABLE books (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL, 
+    published INT
+);
